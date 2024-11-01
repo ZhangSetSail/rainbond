001559721aee5170dfedd0894bbbe09c42387742
张启航 <101104760+ZhangSetSail@users.noreply.github.com>
2024-11-01 10:04:57 +0800

    test: test
    Signed-off-by: 张启航 <101104760+ZhangSetSail@users.noreply.github.com>

--- builder/sources/image.go
+++ builder/sources/image.go
@@ -466,7 +466,7 @@ func ImageBuild(arch, contextDir, cachePVCName, cacheMode, RbdNamespace, Service
 		Command:   []string{"buildctl-daemonless.sh"},
 		Env: []corev1.EnvVar{{
 			Name:  "BUILDCTL_CONNECT_RETRIES_MAX",
-			Value: "20",
+			Value: "200",
 		},
 		},
 		Args: []string{
